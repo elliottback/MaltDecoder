870df59b61173a827195185b81483a3c79de2c36
elliottback <elliottback@gmail.com>
2021-07-31 21:57:45 +0900

    fix: publish release as zip

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = {
     mode: "production",
@@ -24,6 +25,11 @@ module.exports = {
                 { from: path.resolve(__dirname, 'data') + '/manifest.json', to: path.resolve(__dirname, 'dist') + '/manifest.json' },
                 { from: path.resolve(__dirname, 'data') + '/data.json', to: path.resolve(__dirname, 'dist') + '/data.json' }
             ]
+        }),
+        new ZipPlugin({
+          // OPTIONAL: defaults to the Webpack output filename (above) or,
+          // if not present, the basename of the path
+          filename: 'MaltDecoder.zip',
         })
     ]
 };
