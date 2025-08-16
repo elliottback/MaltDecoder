3c84b1fdf15e1d0219f2b71487bf5458ce252e24
elliottback <elliottback@gmail.com>
2025-08-16 16:34:15 +0900

    fix test?

--- test/test-regex-works-for-all.js
+++ test/test-regex-works-for-all.js
@@ -13,10 +13,7 @@ describe('Regex Covers All Defined Keys', function () {
         for (let key in json) {
             //we need to give it a bottle code, so let's say 150
             let hypotheticalRelease = key + ".150";
-            console.log( "hypotheticalRelease=" + hypotheticalRelease);
-
             let match = hypotheticalRelease.match( regex );
-            console.log( "regex=", regex, "; match=", match);
 
             expect(match).to.be.an('array').and.to.have.lengthOf(3);
             expect(match[0]).to.equal(hypotheticalRelease);
